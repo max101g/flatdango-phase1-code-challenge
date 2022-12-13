4dd1e00d2bfea2fd916eb9c4eb12031c75b4c616
max101g <maxgithinji101@gmail.com>
2022-12-13 07:53:24 +0300

    new updates

---
+++ README.md
@@ -0,0 +1,14 @@
+ ### The Fladango Movie Ticket Web App ###
+
+ The flatdango is a movie ticketing web application that allows its users to view on the latest movies that may be debuting on the screens of cinemas. The user gets a chance to buy the ticket online rather than go to get the ticket physically.
+
+ ### How Flatdango operates###
+ On the left, the web application displays a list of movies in a list.
+ Elements of the list are clickable and if the user hovers over the list the cursor canges into a pointer.
+ If any of the element is clicked, movie details are dispalyed on the left; this is inclusive of a gif,movie title, movies description, and number of tickets.
+ Additionally, there is a button that allows the user to buy a ticket.
+
+ ### To add Extra Delivarable ###
+
+ To add a button that allows one to post a movie
+ To add a button that allows one to delete a movie
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// create a DOMContentLoaded event listene
+// create a DOMContentLoaded event listener
 document.addEventListener ('DOMContentLoaded', ()=> {
     displayMovie ();
-    //postMovies ()
 })
 
 //Create a function for GET fetch
@@ -26,6 +25,8 @@ function myMoviesList (movies) {
     listDiv.innerHTML = movies.name
     favouritesList.appendChild (listDiv)
     listDiv.style.cursor = 'pointer'
+
+//Give the list a click event listener
     listDiv.addEventListener ('click', (e)=> {
         e.preventDefault ()
         const cinemaPoster = document.querySelector ('img#image')
@@ -38,6 +39,10 @@ function myMoviesList (movies) {
         movieTime.innerHTML = movies.time
         const ticketNumbers= document.querySelector ('#ticket-numbers')
         ticketNumbers.innerHTML = movies.tickets
+
+
+
+//Give the button a click event && allow it to decrement everytime its clicked
         const buyTicketBtn = document.querySelector ('#btn')
         buyTicketBtn.addEventListener ('click', (e) => {
             e.preventDefault ()
@@ -46,12 +51,41 @@ function myMoviesList (movies) {
             newTickets.innerHTML = i
             if (i < 0){
                 return (newTickets.innerHTML = 'Sold Out')
-            } else if (i <= 0) {
-                alert ('Sorry, Your Fav Movie is Sold Out')
-            }
+            } //else if (i < 1 && i === 0) {
+            //     alert ('Sorry, Your Favourite Movies is Sold Out !!!')
+            // }
         })
-        //console.log (i)
     })
 };
 
-//const list = document.querySelector ()
+//Extra Delivarables
+
+// const editBtn = document.querySelector ('.extra')
+// editBtn.addEventListener ('click', myEditFun)
+
+
+// function myEditFun () {
+//     alert
+// }
+
+
+// let post = {
+//     method: 'POST',
+//     Headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify ({
+//         name:'',
+//         time: '',
+//         tickets: '',
+//         poster: '',
+//         description: ''
+//     })
+// }
+
+// function editer () {
+//     fetch ('http://localhost:3000/movies', post)
+//     .then ((resp)=> resp.json() )
+//     .then (postMovies)
+//     console.log (postMovies)
+// }
